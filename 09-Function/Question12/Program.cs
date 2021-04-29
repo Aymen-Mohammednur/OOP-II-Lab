9bf69ebf7b85bcb24739520e188e6f701e8a92d8
Aymen-Mohammednur <aymenmohammednur12@gmail.com>
2021-04-29 12:34:10 +0300

    Finished function

--- 09-Function/Question12/Program.cs
+++ 09-Function/Question12/Program.cs
@@ -6,7 +6,18 @@ namespace Question12
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32( Console.ReadLine() );
+        
+            Console.WriteLine($"The Fibonacci of {num} is {Fibonacci(num)}");
+        }
+
+        public static int Fibonacci(int num)
+        {
+            if (num <= 2)
+                return 1;
+            else
+                return Fibonacci( num - 1 ) + Fibonacci( num - 2 );
         }
     }
 }
